2812b3e54315c4b8a1bf5bb66f8b6cbb101b6200
Scott Olsen <scott.t.olsen@gmail.com>
2012-08-30 21:41:15 +0000

    Rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nobodyfuckthatguy::Application.config.secret_token = '4393e575dfec68c7193f60a0e163b3ce7b69c0e26cd22707100c0c5cd05944e8e94c071fa5366cb5202415939e71ff7e072e5a03ab5ec7962dfc685936aaf1f8'
